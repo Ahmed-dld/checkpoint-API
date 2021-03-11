d6fcf19e2775e8eb8a79fff8e3755dd37653cd24
ahmed daldoul <ahmed.daldoul@esprit.tn>
2021-03-11 01:49:10 +0100

    checkpoint-API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import UserList from "./UserList";
+import { Card } from "react-bootstrap";
 
 function App() {
+  const listOfUser = UserList();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1> List Of Users </h1>
+            <div className='card_cont' >
+            {listOfUser.map(user=><div key={user.id} className='card_cont' > 
+            <Card className="user" style={{ width: '16rem' }}>
+              <Card.Body>
+                <Card.Title><span>Name :</span> {user.name}</Card.Title>
+                <Card.Title><span>Username :</span> {user.username}</Card.Title>
+                <Card.Title><span>E-mail :</span> {user.email}</Card.Title>
+                <Card.Title><span>Adress :</span> {user.address.street} ,
+                                      {user.address.suite} ,
+                                      {user.address.city} ,
+                                      {user.address.zipcode}.</Card.Title>
+                <Card.Title><span>Phone :</span> {user.phone}</Card.Title>
+                <Card.Title><span>Web-Site :</span> {user.website}</Card.Title>
+                <Card.Title><span>Company Name :</span> {user.company.name}</Card.Title>
+                <Card.Title><span>Company Catch Phrase :</span> {user.company.catchPhrase}</Card.Title>
+                <Card.Title><span>Company Bs:</span> {user.company.bs}</Card.Title>
+              </Card.Body>
+            {/* <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup> */}
+          </Card>
+          </div>)
+            }
+           </div> 
     </div>
   );
 }
